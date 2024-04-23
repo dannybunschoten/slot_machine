ba41c2833168b986b5684f4aea378b1068e198bd
dannybunschoten <danny.bunschoten02@gmail.com>
2024-04-23 20:05:57 +0200

    tiny margin improvals for phone usage

--- components/RollBar.tsx
+++ components/RollBar.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 
 export default function RollBar({values, isRolling, oldOffSet, newOffSet}: {values: string[], isRolling: boolean, oldOffSet: number, newOffSet: number}) {
   return (
-    <div className={`bg-black rounded m-3 lg:m-5 p-2 lg:p-2 h-40 lg:h-80 overflow-hidden text-clip border-solid border-2 lg:border-8 border-yellow-700`}>
+    <div className={`bg-black rounded m-3 lg:m-5 p-2 lg:p-2 h-36 lg:h-80 overflow-hidden text-clip border-solid border-2 lg:border-8 border-yellow-700`}>
       <div
         className={`text-4xl lg:text-8xl text-center leading-[2.5rem] lg:leading-[6rem] text-white ${isRolling ? "animate-roll" : ""}`}
         style={{
