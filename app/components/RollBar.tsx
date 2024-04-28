b350df116aa1c322aebb0d850feadaf960743306
dannybunschoten <danny.bunschoten02@gmail.com>
2024-04-28 17:32:18 +0200

    fixed that the first bar ends before the second

--- app/components/RollBar.tsx
+++ app/components/RollBar.tsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react";
 
-export default function RollBar({values, isRolling, oldOffSet, newOffSet}: {values: string[], isRolling: boolean, oldOffSet: number, newOffSet: number}) {
+export default function RollBar({values, isRolling, oldOffSet, newOffSet, rollTime}: {values: string[], isRolling: boolean, oldOffSet: number, newOffSet: number, rollTime: string}) {
+  
   return (
     <div className={`bg-black rounded m-3 lg:m-5 p-2 lg:p-2 h-36 lg:h-80 overflow-hidden text-clip border-solid border-2 lg:border-8 border-yellow-700`}>
       <div
-        className={`text-4xl lg:text-8xl text-center leading-[2.5rem] lg:leading-[6rem] text-white ${isRolling ? "animate-roll" : ""}`}
+        className={`text-4xl lg:text-8xl text-center leading-[2.5rem] lg:leading-[6rem] text-white ${isRolling ? rollTime : ""}`} 
         style={{
           '--translate-y-from': `-${oldOffSet}rem`,
           '--translate-y': `-${newOffSet}rem`,
