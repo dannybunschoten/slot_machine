8324e35dfaaf52d323e4573eadfad183794fc05b
dannybunschoten <danny.bunschoten02@gmail.com>
2024-08-21 13:19:36 +0200

    revert to old image since optimized Image was giving caching issues

--- app/components/Item.tsx
+++ app/components/Item.tsx
@@ -1,41 +1,15 @@
-import Image from "next/image";
-import cherries from "@/public/cherries.png";
-import clover from "@/public/clover.png";
-import diamond from "@/public/diamond.png";
-import grapes from "@/public/grapes.png";
-import horseshoe from "@/public/horseshoe.png";
-import lemon from "@/public/orange.png";
-import seven from "@/public/seven.png";
-import watermelon from "@/public/watermelon.png";
+import { Fruit } from "../commons/fruits";
 
-const map = {
-  cherries: cherries,
-  clover: clover,
-  diamond: diamond,
-  grapes: grapes,
-  horseshoe: horseshoe,
-  lemon: lemon,
-  seven: seven,
-  watermelon: watermelon,
-} as const;
-
-type ValueKey = keyof typeof map;
-
-export function Item({
-  item,
-  isWinning,
-}: {
-  item: ValueKey;
-  isWinning: boolean;
-}) {
+export function Item({ item, isWinning }: { item: Fruit; isWinning: boolean }) {
   return (
     <div
       className={`flex items-center justify-center p-2 lg:p-4 ${isWinning ? "animate-[glow_0.7s_alternate_infinite]" : ""}`}
     >
-      <Image
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img
+        src={`/${String(item)}.png`}
         alt={item}
         className="h-[60px] w-[60px] lg:h-[96px] lg:w-[96px] rotate-180"
-        src={map[item]}
       />
     </div>
   );
